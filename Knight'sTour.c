dbf692873bca0e9d8e100ff2e6a6089a1796aa30
Mateus Augusto Gomes <mateus7.dev@gmail.com>
2022-08-27 18:22:13 -0300

    retirar linhas de codigo poluidas

--- Knight'sTour.c
+++ Knight'sTour.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define N 8
-//quantidade de casas visitadas = contador_casas
-//quantidade de casas retrocedidas contador_retrocedidas
+
 void result_tabuleiro(int tabuleiro[N][N]);
-//inta(int pos1, int pos2, int completo, int tabuleiro[N][N], int horizontal[N], int vertical);
 int movimento_valido(int posicao_x , int posicao_y, int tabuleiro[N][N]);
-//int backtracking(int posicao_x, int posicao_y, int completo, int tabuleiro[N][N], int horizontal[], int vertical[]);
 int backtracking(int posicao_x, int posicao_y, int completo, int tabuleiro[N][N], int horizontal[], int vertical[], unsigned long int *casas, unsigned long int *retorno);
 void contador_casas(unsigned long int contador_casas);
 void contador_retrocedidas(unsigned long int contador_retrocedidas);
@@ -27,26 +24,20 @@ int main(){
     	for(int j=0;j<N;j++)
             tabuleiro[i][j]=-1;
             
-    //vetores que, quando combinados, representam todos os movimentos possiveis para o knight
-    //exemplo, mover_horizontal[0] e mover_vertical[0] representa o movimento m1
    	int mover_horizontal[]={-1,-2,-2,-1,1,2,2,1};
    	int mover_vertical[]={2,1,-1,-2,-2,-1,1,2};
    	
-    //testanto outra casa alem da [1,1]
    	tabuleiro[5][4]=0;
-    //unsigned long int casas=0, tentativas=0;
        if(backtracking(5,4,1,tabuleiro,mover_horizontal,mover_vertical,pont_casas,pont_retrocedidas)==0){
             printf("Sem solução.\n");
        	    return 0;
    	    }else
             result_tabuleiro(tabuleiro);
     
-    //printf("\nCasas Visitadas:%lu\nCasas Retrocedidas:%lu\n",pont_casas,pont_retrocedidas);
     printf("\nCasas Visitadas:%lu\nCasas Retrocedidas:%lu\n",casas_visitadas,casas_retrocedidas);
 	return 0;
 }  
     	
-//int backtracking(int posicao_x, int posicao_y, int completo, int tabuleiro[N][N], int horizontal[], int vertical[]){
 int backtracking(int posicao_x, int posicao_y, int completo, int tabuleiro[N][N], int horizontal[], int vertical[], unsigned long int *casas, unsigned long int *retorno){
 	int  horizontal1, vertical1;
 	unsigned long int contador_casas=0, contador_retrocedidas=0;
@@ -55,11 +46,9 @@ int backtracking(int posicao_x, int posicao_y, int completo, int tabuleiro[N][N]
 	for(int n=0; n<N ;n++){
         horizontal1=posicao_x+horizontal[n];
         vertical1=posicao_y+vertical[n];
-    	//criar funcao para verificar posicao atual, retorna 1 se estiver de boa
         if(movimento_valido(horizontal1,vertical1,tabuleiro)){
             tabuleiro[horizontal1][vertical1]=completo;
         	if(backtracking(horizontal1, vertical1, completo+1, tabuleiro, horizontal, vertical, casas, retorno)==1){
-        	//if(backtracking(horizontal1, vertical1, completo+1, tabuleiro, horizontal, vertical)==1){
         	    *casas=*casas+1;
                 return 1;
         	}
